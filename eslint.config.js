ac199f7f39270088bbfd645c4f3a936ba529b034
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-08-27 11:47:41 +0300

    install `eslint-plugin-better-tailwindcss`

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
 import globals from "globals";
 import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
-import vuePrettierEslintConfig from "@vue/eslint-config-prettier/skip-formatting";
 import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
+import vuePrettierEslintConfig from "@vue/eslint-config-prettier/skip-formatting";
+import betterTailwindcss from "eslint-plugin-better-tailwindcss";
+import vueEslintParser from "vue-eslint-parser";
+import htmlEslintParser from "@html-eslint/parser";
 
 export default defineConfigWithVueTs(
   js.configs.recommended,
@@ -11,6 +14,34 @@ export default defineConfigWithVueTs(
     files: ["src/**/*.ts", "src/**/*.vue"],
     languageOptions: { ecmaVersion: "latest", globals: { ...globals.browser } },
     rules: { "no-console": ["warn"] },
+  },
+  { files: ["**/*.html"], languageOptions: { parser: htmlEslintParser } },
+  { files: ["**/*.vue"], languageOptions: { parser: vueEslintParser } },
+  {
+    plugins: { "better-tailwindcss": betterTailwindcss },
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "src/assets/styles.css",
+      },
+    },
+    rules: {
+      // Stylistic rules.
+      "better-tailwindcss/enforce-consistent-line-wrapping": ["warn", { printWidth: 120 }],
+      "better-tailwindcss/enforce-consistent-class-order": ["warn", { order: "official" }],
+      "better-tailwindcss/enforce-consistent-variable-syntax": ["warn"],
+      "better-tailwindcss/enforce-consistent-important-position": ["warn"],
+      "better-tailwindcss/enforce-shorthand-classes": ["warn"],
+      "better-tailwindcss/no-duplicate-classes": ["warn"],
+      "better-tailwindcss/no-deprecated-classes": ["warn"],
+      "better-tailwindcss/no-unnecessary-whitespace": ["warn"],
+
+      // Correctness rules.
+      // For some reason, some Nuxt UI classes aren't recognized by the plugin.
+      // So keep the `no-unregistered-classes` rule disabled for now.
+      "better-tailwindcss/no-unregistered-classes": ["off"],
+      "better-tailwindcss/no-conflicting-classes": ["off"],
+      "better-tailwindcss/no-restricted-classes": ["off"],
+    },
   },
   vuePrettierEslintConfig,
   vueTsConfigs.recommended,
