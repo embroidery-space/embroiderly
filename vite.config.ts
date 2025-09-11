98f8d3de5fbfbd3832568e314d20ca9a1ad319f1
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-09-11 11:09:20 +0300

    use default build target (`baseline-widely-available`)

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_ENV_*"],
   server: { port: 1420, strictPort: true, watch: { ignored: ["src-tauri/**"] } },
   build: {
-    target: [process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome107" : "safari16"],
     minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
     chunkSizeWarningLimit: 1000,
