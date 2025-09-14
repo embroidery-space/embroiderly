e151675f1edecb42ed2aec2ccce34fc1314e5d83
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-09-14 15:09:15 +0300

    ci: fix sentry debug symbols uploading (#305)

--- crates/embroiderly-publish/src/logger.rs
+++ crates/embroiderly-publish/src/logger.rs
@@ -2,7 +2,7 @@ use embroiderly_logger::APPLICATION_LOG_LEVEL;
 
 /// Initializes the logger.
 pub fn init() -> anyhow::Result<()> {
-  let dispatch = embroiderly_logger::init_sidecar_logger("embroiderly-publish")?
+  let dispatch = embroiderly_logger::init_sidecar_logger("embroiderly_publish")?
     .level_for("embroiderly_publish", APPLICATION_LOG_LEVEL.to_level_filter());
   dispatch.apply()?;
 
