e8df7439164eb2a2b247e5aacd7ac2adb402586c
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-09-15 15:08:10 +0300

    refactor: extract startup function into a separate module

---
+++ app/src-tauri/src/startup/mod.rs
@@ -0,0 +1,12 @@
+mod auto_save;
+pub use auto_save::*;
+
+mod file_associations;
+pub use file_associations::*;
+
+mod patterns;
+#[allow(unused_imports)]
+pub use patterns::*;
+
+mod webview;
+pub use webview::*;
