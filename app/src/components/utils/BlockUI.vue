360ce864aab529a8886e77a09798ba6ba71a4d93
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-11-18 10:10:02 +0200

    move `BlockUI` component to utility components

---
+++ app/src/components/utils/BlockUI.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :aria-busy="blocked" class="relative">
+    <div v-if="blocked" class="absolute top-0 left-0 z-0 size-full bg-black/50"></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  interface BlockUIProps {
+    /** Whether the UI is blocked. */
+    blocked?: boolean;
+  }
+
+  defineProps<BlockUIProps>();
+</script>
