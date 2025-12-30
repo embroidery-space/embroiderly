61de3b2501502d7e7623123636f05e2f265750ee
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-12-30 14:28:22 +0200

    fix: specify correct pattern info translations (#451)

--- app/src/modules/pattern-editor/components/pattern-info/PatternInfoForm.vue
+++ app/src/modules/pattern-editor/components/pattern-info/PatternInfoForm.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-col gap-y-2">
-    <UFormField :label="$t('label-pattern-title')" class="w-full">
+    <UFormField :label="$t('pattern-info-title')" class="w-full">
       <UInput v-model="patternInfo.title" data-testid="pattern-title-input" class="w-full" />
     </UFormField>
-    <UFormField :label="$t('label-pattern-author')" class="w-full">
+    <UFormField :label="$t('pattern-info-author')" class="w-full">
       <UInput v-model="patternInfo.author" data-testid="pattern-author-input" class="w-full" />
     </UFormField>
-    <UFormField :label="$t('label-pattern-copyright')" class="w-full">
+    <UFormField :label="$t('pattern-info-copyright')" class="w-full">
       <UInput v-model="patternInfo.copyright" data-testid="pattern-copyright-input" class="w-full" />
     </UFormField>
-    <UFormField :label="$t('label-pattern-description')" class="w-full">
+    <UFormField :label="$t('pattern-info-description')" class="w-full">
       <UTextarea
         v-model="patternInfo.description"
         data-testid="pattern-description-textarea"
