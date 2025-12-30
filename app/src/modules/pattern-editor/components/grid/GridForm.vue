9935bed0d5b8c55a5910d3491967619f7caf4148
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-12-30 14:28:07 +0200

    refactor: major lines should go before minor ones in the Grid Properties modal (#450)

--- app/src/modules/pattern-editor/components/grid/GridForm.vue
+++ app/src/modules/pattern-editor/components/grid/GridForm.vue
@@ -12,23 +12,6 @@
       </UFormField>
     </div>
 
-    <FormFieldset :legend="$t('grid-minor-lines')">
-      <div class="grid grid-cols-2 gap-4">
-        <UFormField v-bind="$ta('grid-thickness')">
-          <UInputNumber
-            v-model="grid.minorLines.thickness"
-            data-testid="grid-minor-lines-thickness-input"
-            orientation="vertical"
-            :min="0.001"
-            :step="0.01"
-          />
-        </UFormField>
-        <UFormField :label="$t('grid-color')">
-          <ColorPicker v-model="grid.minorLines.color" data-testid="grid-minor-lines-color-input" />
-        </UFormField>
-      </div>
-    </FormFieldset>
-
     <FormFieldset :legend="$t('grid-major-lines')">
       <div class="grid grid-cols-2 gap-4">
         <UFormField v-bind="$ta('grid-thickness')">
@@ -42,6 +25,23 @@
         </UFormField>
         <UFormField :label="$t('grid-color')">
           <ColorPicker v-model="grid.majorLines.color" data-testid="grid-major-lines-color-input" />
+        </UFormField>
+      </div>
+    </FormFieldset>
+
+    <FormFieldset :legend="$t('grid-minor-lines')">
+      <div class="grid grid-cols-2 gap-4">
+        <UFormField v-bind="$ta('grid-thickness')">
+          <UInputNumber
+            v-model="grid.minorLines.thickness"
+            data-testid="grid-minor-lines-thickness-input"
+            orientation="vertical"
+            :min="0.001"
+            :step="0.01"
+          />
+        </UFormField>
+        <UFormField :label="$t('grid-color')">
+          <ColorPicker v-model="grid.minorLines.color" data-testid="grid-minor-lines-color-input" />
         </UFormField>
       </div>
     </FormFieldset>
