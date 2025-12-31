0a8ca5adf418293ad65a281270688b8aa05acdfb
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-12-31 08:28:04 +0200

    remove extra styles form the `PaletteListItem` component

--- app/src/modules/pattern-editor/components/palette/PaletteListItem.vue
+++ app/src/modules/pattern-editor/components/palette/PaletteListItem.vue
@@ -33,15 +33,3 @@
 
   const { paletteItem, selected, displaySettings } = defineProps<PaletteItemProps>();
 </script>
-
-<style scoped>
-  .palette-list-item-shadow-white {
-    outline: 2px solid white;
-    outline-offset: -4px;
-  }
-
-  .palette-list-item-shadow-black {
-    outline: 2px solid black;
-    outline-offset: -4px;
-  }
-</style>
