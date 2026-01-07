3ed9569de2a70d18378bb0c499f4615806a72bad
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-01-07 15:46:25 +0200

    move components to the theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import VPSwiper from "@cssnr/vitepress-swiper";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 
-import DownloadWidget from "../components/DownloadWidget.vue";
+import DownloadWidget from "./components/DownloadWidget.vue";
 
 import "@cssnr/vitepress-swiper/style.css";
 import "./assets/styles.css";
