7f98a3a7d0df9a3a0bfb718f19b5afc6f55c8035
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-01-24 08:23:53 +0200

    update exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,13 +1,9 @@
-export { default as UApp, type AppProps as UAppProps, type AppSlots as UAppSlots } from "./components/App.vue";
-export {
-  default as UButton,
-  type ButtonProps as UButtonProps,
-  type ButtonSlots as UButtonSlots,
-} from "./components/Button.vue";
-export { default as UButtonIcon, type ButtonIconProps as UButtonIconProps } from "./components/ButtonIcon.vue";
-export {
-  default as UTooltip,
-  type TooltipProps as UTooltipProps,
-  type TooltipEmits as UTooltipEmits,
-  type TooltipSlots as UTooltipSlots,
-} from "./components/Tooltip.vue";
+// Layout.
+export { default as App, type AppProps, type AppSlots } from "./components/App.vue";
+
+// Element.
+export { default as Button, type ButtonProps, type ButtonSlots } from "./components/Button.vue";
+export { default as UButtonIcon, type ButtonIconProps } from "./components/ButtonIcon.vue";
+
+// Overlay.
+export { default as Tooltip, type TooltipProps, type TooltipEmits, type TooltipSlots } from "./components/Tooltip.vue";
