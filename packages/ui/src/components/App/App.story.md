25e29c92844286e45b5b3cac9f0493255a4014fa
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-01-25 09:02:48 +0200

    update `App` component by extending props

--- packages/ui/src/components/App/App.story.md
+++ packages/ui/src/components/App/App.story.md
@@ -9,9 +9,9 @@ Wraps the application to provide global configurations.
 
 ## Usage
 
-This component implements Reka UI [ConfigProvider](https://reka-ui.com/docs/utilities/config-provider) and [TooltipProvider](https://reka-ui.com/docs/components/tooltip#provider) to provide global configuration to all components.
+This component implements Reka UI [`ConfigProvider`](https://reka-ui.com/docs/utilities/config-provider) and [`TooltipProvider`](https://reka-ui.com/docs/components/tooltip#provider) to provide global configuration to all components.
 
-Wrap your entire application with the `App` component in the root file:
+Wrap the entire application with the `App` component in the root file:
 
 ```vue
 <template>
--- packages/ui/src/components/App/App.vue
+++ packages/ui/src/components/App/App.vue
@@ -1,18 +1,13 @@
-<template>
-  <ConfigProvider>
-    <TooltipProvider>
-      <slot />
-    </TooltipProvider>
-  </ConfigProvider>
-</template>
-
 <script setup lang="ts">
-  import { ConfigProvider, TooltipProvider } from "reka-ui";
-  import { provide, toRef } from "vue";
+  import { reactivePick } from "@vueuse/core";
+  import type { ConfigProviderProps, TooltipProviderProps } from "reka-ui";
+  import { ConfigProvider, TooltipProvider, useForwardProps } from "reka-ui";
+  import { provide, toRef, useId } from "vue";
 
   import { GLOBAL_PORTAL, PORTAL_TARGET_INJECTION_KEY } from "../../composables/usePortal.ts";
 
-  export interface AppProps {
+  export interface AppProps extends Omit<ConfigProviderProps, "useId"> {
+    tooltip?: TooltipProviderProps;
     portal?: boolean | string | HTMLElement;
   }
 
@@ -24,6 +19,17 @@
     portal: GLOBAL_PORTAL,
   });
 
+  const configProps = useForwardProps(reactivePick(props, "dir", "locale", "scrollBody"));
+  const tooltipProps = toRef(() => props.tooltip);
+
   const portal = toRef(() => props.portal);
   provide(PORTAL_TARGET_INJECTION_KEY, portal);
 </script>
+
+<template>
+  <ConfigProvider v-bind="configProps" :use-id="() => useId()">
+    <TooltipProvider v-bind="tooltipProps">
+      <slot />
+    </TooltipProvider>
+  </ConfigProvider>
+</template>
