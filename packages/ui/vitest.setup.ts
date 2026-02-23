616920e3071eb618fe0d9bc9f960eebe03bc0d64
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-02-23 15:11:56 +0200

    load icons offline for testing

--- packages/ui/vitest.setup.ts
+++ packages/ui/vitest.setup.ts
@@ -1,6 +1,11 @@
+import { addCollection } from "@iconify/vue";
+import lucideIcons from "@iconify-json/lucide/icons.json";
 import { beforeEach } from "vitest";
 
 import "./src/index.css";
+
+// Load Lucide icons offline.
+addCollection(lucideIcons);
 
 beforeEach(() => {
   // @ts-expect-error `vue3-snapshot-serializer` currently doesn't provide any typings.
