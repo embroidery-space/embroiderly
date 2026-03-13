4bc19298d35a95127fd979b8c132f80042cd5dde
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-03-13 09:54:21 +0200

    chore: migrate to a JavaScript config for `lint-staged`

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  "*.md": ["markdownlint-cli2 --fix", "oxfmt"],
+  "*.rs": () => {
+    // Clippy and Rustfmt don't support multiple files formatting via CLI.
+    // Therefore, run them on the entire codebase.
+    return ["cargo clippy --locked --fix --allow-dirty -- -D warnings", "cargo +nightly fmt"];
+  },
+  "!(*.md|*.rs)": ["oxlint --fix --format stylish", "eslint --fix --concurrency auto", "oxfmt"],
+};
