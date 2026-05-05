91b1430d403bd76281edea2141d6457c6126272d
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-05-05 10:13:57 +0300

    add a missing origin verification in the service worker

--- app/src/sw.ts
+++ app/src/sw.ts
@@ -12,5 +12,9 @@ precacheAndRoute(self.__WB_MANIFEST);
 // The `SKIP_WAITING` listener gives us the "ask before updating" UX.
 // Without it, the new SW would activate on every reload by default.
 self.addEventListener("message", (event) => {
+  // The origin check is not really needed in for service workers, as they are already origin-scoped, which is enforced directly by the browsers.
+  // However, this check satisfies CodeQL static analysis, so that no warning alerts are reported.
+  if (event.origin !== self.location.origin) return;
+
   if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
 });
