77de3e46f13748bf28b5769bdb5a65b77679a1b3
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2026-05-13 10:43:49 +0300

    feat: add demo patterns

--- app/src/components/AppHeader.vue
+++ app/src/components/AppHeader.vue
@@ -36,14 +36,6 @@ const menus = computed<MenubarMenu[]>(() => {
     items: [
       [
         {
-          label: fluent.$t("app-menu-file-open"),
-          shortcut: "Control+O",
-          async onSelect() {
-            const patternId = await patternFileStore.openPattern();
-            if (patternId) patternFileStore.switchPattern(patternId);
-          },
-        },
-        {
           label: fluent.$t("app-menu-file-create"),
           shortcut: "Control+N",
           onSelect() {
@@ -54,6 +46,49 @@ const menus = computed<MenubarMenu[]>(() => {
               },
             });
           },
+        },
+        {
+          label: fluent.$t("app-menu-file-open"),
+          shortcut: "Control+O",
+          async onSelect() {
+            const patternId = await patternFileStore.openPattern();
+            if (patternId) patternFileStore.switchPattern(patternId);
+          },
+        },
+        {
+          label: fluent.$t("app-menu-file-open-demo"),
+          children: [
+            [
+              {
+                label: fluent.$t("demo-pattern-piggies"),
+                async onSelect() {
+                  const id = await patternFileStore.openPattern({ demo: "Piggies.oxs" });
+                  if (id) patternFileStore.switchPattern(id);
+                },
+              },
+              {
+                label: fluent.$t("demo-pattern-festive-capy"),
+                async onSelect() {
+                  const id = await patternFileStore.openPattern({ demo: "Festive Capy.oxs" });
+                  if (id) patternFileStore.switchPattern(id);
+                },
+              },
+              {
+                label: fluent.$t("demo-pattern-pumpkin-cupcake"),
+                async onSelect() {
+                  const id = await patternFileStore.openPattern({ demo: "Pumpkin Cupcake.oxs" });
+                  if (id) patternFileStore.switchPattern(id);
+                },
+              },
+              {
+                label: fluent.$t("demo-pattern-whisper-of-the-board"),
+                async onSelect() {
+                  const id = await patternFileStore.openPattern({ demo: "The Whisper of the Board.xsd" });
+                  if (id) patternFileStore.switchPattern(id);
+                },
+              },
+            ],
+          ],
         },
       ],
       [
